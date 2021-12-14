fee210976f129ac112a65b9e120da369b117bc27
Mosi8 <mauriziomosi8@gmail.com>
2021-12-14 21:25:49 +0100

    scorrimento funziona

--- myscript.js
+++ myscript.js
@@ -60,3 +60,89 @@ let itemActive = document.getElementById('item-grande-'+ elementActive);
 itemActive.classList.remove('hidden')
 itemActive.classList.add('active');
 
+// tasti up e down 
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+
+// scendere immagine 
+down.addEventListener('click', function() {
+    fotoActive++;
+    elementActive++;
+    // foto grande 
+    if (elementActive == items.length) {
+        elementActive = 0;
+        itemActive.classList.add('hidden')
+        itemActive.classList.remove('active');
+        itemActive = document.getElementById('item-grande-'+ elementActive);
+        itemActive.classList.remove('hidden');
+        itemActive.classList.add('active');
+    }else{
+        itemActive.classList.add('hidden')
+        itemActive.classList.remove('active');
+        itemActive = document.getElementById('item-grande-'+ elementActive);
+        itemActive.classList.remove('hidden');
+        itemActive.classList.add('active');
+    }
+
+    // foto piccola 
+    if (fotoActive == items.length) {
+        fotoActive = 0;
+        fotoPiccolaActive.classList.add('ms_oscura')
+        fotoPiccolaActive.classList.remove('ms_no_oscura');
+        fotoPiccolaActive.classList.remove('ms_border');
+        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
+        fotoPiccolaActive.classList.remove('ms_oscura');
+        fotoPiccolaActive.classList.add('ms_no_oscura');
+        fotoPiccolaActive.classList.add('ms_border');
+    }else{
+        fotoPiccolaActive.classList.add('ms_oscura')
+        fotoPiccolaActive.classList.remove('ms_no_oscura');
+        fotoPiccolaActive.classList.remove('ms_border');
+        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
+        fotoPiccolaActive.classList.remove('ms_oscura');
+        fotoPiccolaActive.classList.add('ms_no_oscura');
+        fotoPiccolaActive.classList.add('ms_border'); 
+    }
+});
+
+
+// salire immagine 
+up.addEventListener('click', function() {
+    fotoActive--;
+    elementActive--;
+    // foto grande 
+    if (elementActive == 0-1) {
+        elementActive = items.length-1;
+        itemActive.classList.add('hidden')
+        itemActive.classList.remove('active');
+        itemActive = document.getElementById('item-grande-'+ elementActive);
+        itemActive.classList.remove('hidden');
+        itemActive.classList.add('active');
+    }else{
+        itemActive.classList.add('hidden')
+        itemActive.classList.remove('active');
+        itemActive = document.getElementById('item-grande-'+ elementActive);
+        itemActive.classList.remove('hidden');
+        itemActive.classList.add('active');
+    }
+
+    // foto piccola 
+    if (fotoActive == 0 - 1) {
+        fotoActive = items.length-1;
+        fotoPiccolaActive.classList.add('ms_oscura')
+        fotoPiccolaActive.classList.remove('ms_no_oscura');
+        fotoPiccolaActive.classList.remove('ms_border');
+        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
+        fotoPiccolaActive.classList.remove('ms_oscura');
+        fotoPiccolaActive.classList.add('ms_no_oscura');
+        fotoPiccolaActive.classList.add('ms_border');
+    }else{
+        fotoPiccolaActive.classList.add('ms_oscura')
+        fotoPiccolaActive.classList.remove('ms_no_oscura');
+        fotoPiccolaActive.classList.remove('ms_border');
+        fotoPiccolaActive = document.getElementById('items-'+ fotoActive);
+        fotoPiccolaActive.classList.remove('ms_oscura');
+        fotoPiccolaActive.classList.add('ms_no_oscura');
+        fotoPiccolaActive.classList.add('ms_border');
+    }
+});
